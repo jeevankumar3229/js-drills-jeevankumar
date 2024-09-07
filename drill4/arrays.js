0d29c3a15119aa76394675f16984f7354e13b5db
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-07 17:04:44 +0530

    Implement logic for reduce(). reduce() calls a callback function convertToSingleValue() and passes starting value and array element value as arguments to callback function and returns single value as output . callback function convertToSingleValue() adds starting value and array element value

--- drill4/arrays.js
+++ drill4/arrays.js
@@ -54,13 +54,36 @@ function transform(item){
 console.log(map(items, transform))
 
 
-function reduce(elements, cb, startingValue) {
-  // Do NOT use .reduce to complete this function.
+
+// Do NOT use .reduce to complete this function.
   // How reduce works: A reduce function combines all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+function reduce(elements, cb, startingValue) {
+  if(startingValue === undefined){
+    startingValue =elements[0];
+    for(let i=1;i<elements.length;i++){
+      startingValue=cb(startingValue,elements[i])
+    }
+  }
+  else{
+  for(let i=0;i<elements.length;i++){
+    startingValue=cb(startingValue,elements[i])
+  }
 }
+return startingValue;
+}
+
+function convertToSingleValue(starting_value,arrayelement){
+  return starting_value + arrayelement;
+}
+
+console.log(reduce(items, convertToSingleValue,20))
+
+
+
+
 
 function find(elements, cb) {
   // Do NOT use .includes, to complete this function.
