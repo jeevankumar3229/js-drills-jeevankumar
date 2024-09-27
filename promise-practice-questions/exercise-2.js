6add5030f4379fb401f9b86eb396e18a6dec5077
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-27 10:58:12 +0530

    Implement logic for exercise-2.js

---
+++ promise-practice-questions/exercise-2.js
@@ -0,0 +1,45 @@
+/*
+    1. Print out "Program started" at the start of your code
+    2. Create a Promise that resolves after 3 seconds
+    3. Log out the promise while it's pending
+    4. Print out "Program in progress..." as well
+
+    5. Print out "Step 1 complete" when the first promise fulfills
+    6. Have the first promise return another new Promise that will
+       fulfill after 3 seconds with the message: "Step 2 Complete"
+
+    7. Print out the message from the second promise after it
+       fulfills ("Step 2 Complete")
+
+    HINT: Use setTimeout for the delay
+*/
+
+function exercise2() {
+   console.log("Program started")
+   const promise=new Promise(function (resolve, reject) {
+       try {
+           setTimeout(() => {
+               resolve(()=>{
+                  console.log("Step 1 complete")
+                  return new Promise(function(resolve, reject){
+                     setTimeout(()=>{
+                        resolve("Step 2 Complete")
+                     },3000)
+                  })
+               })
+           }, 3000)
+       }
+       catch (error) {
+           reject(error)
+       }
+   })
+   console.log(promise,"Program in progress")
+   return promise
+}
+exercise2().then((data)=>{
+   data().then(data=>{
+      console.log(data)
+   })
+}).catch(error=>{
+   console.log(error)
+})
